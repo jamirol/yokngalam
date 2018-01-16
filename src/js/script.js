d790d0effda10c50e8fda9cf7deaaafc8c5944d9
daniel s <jamirol@onet.eu>
2018-01-16 02:20:23 +0100

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,43 @@
+
+
+ $(document).ready(function(){
+    $('.menu-toggler').on('click',function(){
+      $('nav').slideToggle().css("display", "block");
+    });
+  
+  
+  
+  
+  $('a').animate({
+    scrollTop: $('.menu-toggler').offset().top
+  }, 2100);
+  
+  
+  
+  $('.form-info','.form-mail').validate({
+        
+    rules: {
+      email: {
+         required: true,
+         email: true
+      }
+    }, //koniec literału obiektowego rules
+    messages: {
+       email: {
+          required: "Podaj adres e-mail.",
+          email: "To nie jest prawidłowy adres e-mail."
+        }
+    },
+    // // errorPlacement: function(error, element) { 
+    //     if ( element.is(":radio") || element.is(":checkbox")) {
+    //        error.appendTo( element.parent()); 
+    //      } else {
+    //        error.insertAfter(element);
+    //      } 
+    // // }
+  
+   }); // koniec funkcji validate 
+  
+  
+  
+   });
